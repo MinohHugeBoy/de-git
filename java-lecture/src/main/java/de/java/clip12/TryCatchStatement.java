6a877c9b768dfadcb7b91b523a70ce176b3d9b2b
MinohHugeBoy <minoh931208@gmail.com>
2024-09-15 10:04:33 +0900

    240915 try exception

---
+++ java-lecture/src/main/java/de/java/clip12/TryCatchStatement.java
@@ -0,0 +1,18 @@
+package de.java.clip12;
+
+import com.sun.xml.internal.org.jvnet.fastinfoset.FastInfosetException;
+
+import java.io.FileNotFoundException;
+
+public class TryCatchStatement {
+    public static void main(String[] args) {
+        throw e;
+        try {
+
+        } catch (Throwable e) {
+            //throw e;
+        } finally {
+
+        }
+    }
+}
